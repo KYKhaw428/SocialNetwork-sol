23d40253ac4e22ed4f6a13c84cc76f49b1628c62
KYKhaw428 <kengyearn@hotmail.com>
2020-02-24 13:19:23 +0800

    Updates

---
+++ precache-manifest.ce1fbf8c048bb1c26c44e8f11e0d61fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f6fb01ad56c2d702f75",
+    "url": "/SocialNetwork-sol/static/css/main.7086127f.chunk.css"
+  },
+  {
+    "revision": "8f6fb01ad56c2d702f75",
+    "url": "/SocialNetwork-sol/static/js/main.8f6fb01a.chunk.js"
+  },
+  {
+    "revision": "cb8832d57d4c953795e3",
+    "url": "/SocialNetwork-sol/static/css/1.307cf745.chunk.css"
+  },
+  {
+    "revision": "cb8832d57d4c953795e3",
+    "url": "/SocialNetwork-sol/static/js/1.cb8832d5.chunk.js"
+  },
+  {
+    "revision": "afb20a8fe19d29ddca80",
+    "url": "/SocialNetwork-sol/static/js/runtime~main.afb20a8f.js"
+  },
+  {
+    "revision": "cc381e69bf408e05efa45cc8d7d0260a",
+    "url": "/SocialNetwork-sol/index.html"
+  }
+];
